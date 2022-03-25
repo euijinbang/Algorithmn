b0d5ed84952f337f76da894250e42c1285e4d3de
euijinbang <euijin.bang@gmail.com>
2022-03-26 00:27:47 +0900

    0325_UPLOAD: 1475_방번호

--- 구현-실버하위/1475_방번호.py
+++ 구현-실버하위/1475_방번호.py
@@ -1,37 +1,34 @@
-num_set = dict()
+N = input()
 
+# 숫자 세트 딕셔너리 생성
+num_set = dict()
 for i in range(10):
     num_set[i] = 1
 
-N = input()
-
 ans = 1
 
-def check_num_set(num_set):
-    for key, value in num_set.items():
-        if value < 0:
-            return False
 
-def add_num_set(old_num_set):
+# 딕셔너리 전체에 1씩 더한다
+def add_num_set():
+    global num_set
     for key, value in num_set.items():
-        value += 1
+        num_set[key] += 1
+
 
 for num in N:
-    print(num_set)
-    if num == '6' and num_set[6] <= 0 and num_set[9] >= 1:
-        num_set[9] -= 1
+    cur_num = int(num)
+    if num_set[cur_num] <= 0:  # 현재 숫자의 남은 판수가 0 이하라면 대체 가능한지 확인
+        if num == '6' and num_set[9] >= 1:
+            num_set[9] -= 1
+        elif num == '9' and num_set[6] >= 1:
+            num_set[6] -= 1
 
-    elif num == '9' and num_set[9] <= 0 and num_set[6] >= 1:
-        num_set[6] -= 1
-
+        else:   # 대체 불가할 경우 세트를 추가
+            ans += 1
+            add_num_set()
+            num_set[cur_num] -= 1
     else:
-        num_set[int(num)] -= 1
-
-    print(check_num_set(num_set))
-    if check_num_set(num_set) == False:
-        ans += 1
-        add_num_set(num_set)
-
+        num_set[cur_num] -= 1
 
 print(ans)
 
