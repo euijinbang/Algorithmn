1e56917060215f7be329debc8573dde47506b4e3
euijinbang <euijin.bang@gmail.com>
2022-04-20 00:27:06 +0900

    UPDATE :0419

--- 0419_트리,이진탐색,자료구조-배열/53. Maximum Subarray.py
+++ 0419_트리,이진탐색,자료구조-배열/53. Maximum Subarray.py
@@ -2,5 +2,17 @@
 remove negative prefix
 sliding window
 O(n)
+https://www.youtube.com/watch?v=5WZl3MMT0Eg
 """
-print([1, 2, 3][:2])
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]    # 부분배열의 합과 비교하여 최댓값을 갱신
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:  # 해당 수 앞까지의 총합이 음수면 0으로 초기화
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub,curSum)
+
+        return maxSub
