4c338d4c12035ee1f8b73a09780c06e09519b4e9
euijinbang <euijin.bang@gmail.com>
2022-05-03 01:06:28 +0900

    Upload : 0502

--- 0502_알고리즘-recursion,backtracking/40. Combination Sum II.py
+++ 0502_알고리즘-recursion,backtracking/40. Combination Sum II.py
@@ -1,1 +1,21 @@
-#흠....어렵네...다시풀어야겠다..
+#흠....어렵네...다시풀어야겠다.
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        #바로 전 인덱스값이 현재 인덱스값과 같으면 현재 브랜치를 선택하지 않는다.
+        candidates.sort()
+        res = []
+        def backtrack(cur, index, target):
+            if target <= 0:
+                if target == 0:
+                    res.append(cur.copy())
+                    return
+
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i-1]:
+                    continue
+                cur.append(candidates[i])
+                backtrack(cur, i+1, target-candidates[i])
+                cur.pop()
+
+        backtrack([], 0, target)
+        return res
