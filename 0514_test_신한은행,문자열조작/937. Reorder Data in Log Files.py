6f292c897da82c146cc5f50f6b9de5890e8eb6d0
euijinbang <euijin.bang@gmail.com>
2022-05-15 00:27:00 +0900

    Upload : 0514

---
+++ 0514_test_신한은행,문자열조작/937. Reorder Data in Log Files.py
@@ -0,0 +1,18 @@
+"""
+lambda 2개 활용
+.isdigit()
+.split()
+문자열 슬라이싱
+"""
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
