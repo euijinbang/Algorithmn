07986d294656e0ae21ca71b12ac9e1d5868ebc8d
euijinbang <euijin.bang@gmail.com>
2022-07-28 01:35:56 +0900

    Update : 0727

--- 0727_array/13. Roman to Integer.py
+++ 0727_array/13. Roman to Integer.py
@@ -1,38 +1,50 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
 
-        main = {"I": 1,
-                "V": 5,
-                "X": 10,
-                "L": 50,
-                "C": 100,
-                "D": 500,
-                "M": 1000}
+        roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        result = 0
 
-        sub = {"IV": 4,
-               "IX": 9,
-               "XL": 40,
-               "XC": 90,
-               "CD": 400,
-               "CM": 900}
+        for i in range(0, len(s)-1):
+            if roman[s[i]] < roman[s[i+1]]:   # 예외사항의 경우
+                result -= roman[s[i]]
+            else:
+                result += roman[s[i]]
 
-        result, i = 0, 0
-        while True:
-            if i == len(s):
-                break
+        return result + roman[s[-1]]    # 마지막 수는 항상 더한다.
 
-            if len(s) == 1 or i == len(s)-1:
-                result += main[s[i]]
-                break
+#         main = {"I": 1,
+#                 "V": 5,
+#                 "X": 10,
+#                 "L": 50,
+#                 "C": 100,
+#                 "D": 500,
+#                 "M": 1000}
 
-            if s[i] + s[i+1] in sub:
-                result += sub[s[i]+s[i+1]]
-                i += 2
-                continue
+#         sub = {"IV": 4,
+#               "IX": 9,
+#               "XL": 40,
+#               "XC": 90,
+#               "CD": 400,
+#               "CM": 900}
 
-            if s[i] in main:
-                result += main[s[i]]
-                i += 1
+#         result, i = 0, 0
+#         while True:
+#             if i == len(s):
+#                 break
 
-        return result
+#             if len(s) == 1 or i == len(s)-1:
+#                 result += main[s[i]]
+#                 break
+
+#             if s[i] + s[i+1] in sub:
+#                 result += sub[s[i]+s[i+1]]
+#                 i += 2
+#                 continue
+
+#             if s[i] in main:
+#                 result += main[s[i]]
+#                 i += 1
+
+#         return result
+
 
